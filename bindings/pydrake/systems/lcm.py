70507830419f7f0e7eda6f150efa9a26870c635b
Eric Cousineau <eric.cousineau@tri.global>
2018-06-01 00:32:50 -0400

    pydrake systems lcm: Enable publishers and subscribers

---
+++ bindings/pydrake/systems/lcm.py
@@ -0,0 +1,33 @@
+from pydrake.systems._lcm_py import *
+
+from pydrake.systems.framework import AbstractValue, Value
+
+
+class PySerializer(SerializerInterface):
+    def __init__(self, lcm_type):
+        SerializerInterface.__init__(self)
+        self._lcm_type = lcm_type
+
+    def CreateDefaultValue(self):
+        return AbstractValue.Make(self._lcm_type())
+
+    def Deserialize(self, buffer, value):
+        msg = self._lcm_type.decode(buffer)
+        value.set_value(msg)
+
+    def Serialize(self, value):
+        assert isinstance(value, AbstractValue)
+        msg = value.get_value()
+        assert isinstance(msg, self._lcm_type)
+        return msg.encode()
+
+
+# Attach methods (as descriptors).
+@classmethod
+def _make_lcm_system(cls, channel, lcm_type, lcm):
+    serializer = PySerializer(lcm_type)
+    return cls(channel, serializer, lcm)
+
+
+LcmPublisherSystem.Make = _make_lcm_system
+LcmSubscriberSystem.Make = _make_lcm_system
---
+++ bindings/pydrake/systems/test/lcm_test.py
@@ -0,0 +1,47 @@
+import pydrake.systems.lcm as mut
+
+import unittest
+
+from robotlocomotion import quaternion_t
+
+from pydrake.lcm import DrakeMockLcm
+from pydrake.systems.framework import AbstractValue
+
+# Create a simple template message.
+MSG = quaternion_t()
+MSG.w, MSG.x, MSG.y, MSG.z = (1, 2, 3, 4)
+
+
+class TestSystemsLcm(unittest.TestCase):
+    def test_serializer(self):
+        dut = mut.PySerializer(quaternion_t)
+        value = dut.CreateDefaultValue()
+        self.assertIsInstance(value.get_value(), quaternion_t)
+        self.assertNotEqual(value.get_value().encode(), MSG.encode())
+        dut.Deserialize(MSG.encode(), value)
+        self.assertEqual(value.get_value().encode(), MSG.encode())
+        raw = dut.Serialize(value)
+        self.assertEqual(raw, MSG.encode())
+
+    def test_subscriber(self):
+        lcm = DrakeMockLcm()
+        dut = mut.LcmSubscriberSystem.Make(
+            channel="TEST_CHANNEL", lcm_type=quaternion_t, lcm=lcm)
+        lcm.InduceSubscriberCallback(
+            channel="TEST_CHANNEL", buffer=MSG.encode())
+        context = dut.CreateDefaultContext()
+        output = dut.AllocateOutput(context)
+        dut.CalcOutput(context, output)
+        actual = output.get_data(0).get_value()
+        self.assertEqual(actual.encode(), MSG.encode())
+
+    def test_publisher(self):
+        lcm = DrakeMockLcm()
+        dut = mut.LcmPublisherSystem.Make(
+            channel="TEST_CHANNEL", lcm_type=quaternion_t, lcm=lcm)
+        dut.set_publish_period(period=0.1)
+        context = dut.CreateDefaultContext()
+        context.FixInputPort(0, AbstractValue.Make(MSG))
+        dut.Publish(context)
+        raw = lcm.get_last_published_message("TEST_CHANNEL")
+        self.assertEqual(raw, MSG.encode())
